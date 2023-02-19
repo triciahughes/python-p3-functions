924ebbd01f249d89463f4319b3ac5b49cc82d837
triciahughes <hughes.a.tricia@gmail.com>
2023-02-19 14:34:19 -0800

    passed

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,23 @@
 
 def greet_programmer():
     pass
+    print('Hello, programmer!')
 
 def greet(name):
     pass
+    name = 'Guido'
+    print(f'Hello, {name}', end='!\n')
 
 def greet_with_default(name="programmer"):
     pass
+    print(f'Hello, {name}', end='!\n')
 
 def add(num1, num2):
     pass
+    num1 = 45
+    num2 = 55
+    return(num1 + num2)
 
 def halve(number):
     pass
+    return(number / 2)
